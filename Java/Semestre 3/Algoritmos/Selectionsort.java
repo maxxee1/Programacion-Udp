2bee15fdbebbf5879d421076dc27f4fbff8d72b1
Maximiliano <130422048+maxxee1@users.noreply.github.com>
2024-04-15 16:20:41 +0000

    .

--- Java/Semestre 3/Algoritmos/Selectionsort.java
+++ Java/Semestre 3/Algoritmos/Selectionsort.java
@@ -1,3 +1,14 @@
 package Algoritmos; 
 import java.util.Arrays;
 
+public class Selectionsort {
+    public static void main(String[] args) {
+        
+        int[] arreglo = {5, 3, 2, 4, 1};
+        int n = arreglo.length;
+
+        for (int i = 0; i < n; i++) {
+            
+        }
+    }
+}

527d4e22d67180af32666be9c7f251b8e5e00a5e
maxxee1 <maximiliano.adonis@gmail.com>
2024-05-08 23:14:52 -0400

    .

---
+++ Java/Semestre 3/stotify/clases/cancion.java
@@ -0,0 +1,43 @@
+package stotify.clases;
+
+public class cancion {
+    private String titulo;
+    private String artista;
+    private int duracion;
+    
+    
+    public cancion (String titulo, String artista, int duracion) {
+        this.titulo = titulo;
+        this.artista = artista;
+        this.duracion = duracion;
+    }
+
+    // Getters
+    public String getTitulo() {
+        return titulo;
+    }
+    
+    public String getArtista() {
+        return artista;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    // Setters
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+}
+
+
+

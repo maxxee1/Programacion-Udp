ca8e4fe0896536d9412347f8693e54fd000bdb1e
Maximiliano <130422048+maxxee1@users.noreply.github.com>
2024-06-14 14:45:54 +0000

    Priority Queue + Lambda fuction

--- Java/Semestre 3/PriorityQueue/DefaulPriority.java
+++ Java/Semestre 3/PriorityQueue/DefaulPriority.java
@@ -5,11 +5,20 @@ class DefaaultPriority {
     public static void main(String[] args) {
 
         PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        PriorityQueue<Integer> lambdapq = new PriorityQueue<Integer>();
 
+        // agregando items a la cola de prioridad
         pq.add(50);
         pq.add(1);
         pq.add(8);
 
+        // agregando items a la cola de prioridad con lambda
+        lambdapq.add(10);
+        lambdapq.add(20);
+        lambdapq.add(30);
+
+
         System.out.println("PriorityQueue: " + pq);
+        System.out.println("PriorityQueue con lambda: " + lambdapq);
     }
 }
